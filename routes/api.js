e7caa5871e11be27d8b0b5c18d026342a9654c1a
ayahkhalilia <ayah.khalilia@stu.shenkar.ac.il>
2025-07-31 13:44:25 +0300

    deleted file

--- routes/api.js
+++
@@ -1,5 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const db=require('../controllers/dbController.js');
-
-module.exports=router;
--- server.js
+++ server.js
@@ -2,21 +2,16 @@ require('dotenv').config({path:'./.env'});
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const apiRoutes=require('./routes/api');
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/api',apiRoutes);
 app.use('/auth', require('./routes/auth'));
 // DB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB error:', err));
-
-// Routes
-
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
